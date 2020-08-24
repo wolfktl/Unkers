dab11a29399d0e2ceeabdf86be44e7dafe4ab9ad
wolfktl <wolf_ktl@mail.ru>
2020-08-24 10:34:27 +0000

    Create printer_instal.sh

---
+++ printer_instal.sh
@@ -0,0 +1,11 @@
+wget https://raw.githubusercontent.com/wolfktl/Unkers/master/Sewoo.tar
+wget https://raw.githubusercontent.com/wolfktl/Unkers/master/GoDEX.tar
+tar xvf GoDEX.tar
+tar xvf Sewoo.tar
+
+
+sudo chmod +x /home/user/GoDEX/install.sh
+sudo chmod +x /home/user/Sewoo/install.sh
+
+sudo /home/user/GoDEX/install.sh
+sudo /home/user/Sewoo/install.sh
